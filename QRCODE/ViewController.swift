cc0250a9f976c82e646c819fc7c967b5cbf9cd49
imranshahmi <s10187013@connect.np.edu.sg>
2020-02-13 13:48:07 +0800

    Initial Commit

---
+++ QRCODE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCODE
+//
+//  Created by MAD2_P01 on 13/2/20.
+//  Copyright © 2020 gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
